7a425890a10fe31c207816697b4bd89d8f5b319c
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2026-08-28 13:27:39 -0700

    chore(release): bind RTX4090 beta candidate

--- tests/test_release_assets.ps1
+++ tests/test_release_assets.ps1
@@ -26,7 +26,7 @@ function Write-Json([string]$Path, [object]$Value) {
 
 $root = Join-Path ([IO.Path]::GetTempPath()) ('ninfer-release-assets-' + [Guid]::NewGuid().ToString('N'))
 New-Item -ItemType Directory -Path $root | Out-Null
-$patchSha = 'ea265776254a62ab5184454ba0163cdf04aad1e5'
+$patchSha = '40580cc703b03197573789923b5866007aba0a68'
 $assetStem = 'ninfer-4090-qwen38-v0.1.0-win-x64'
 
 try {
